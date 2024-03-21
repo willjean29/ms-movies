3bfdabc0a796348c64e7af9a6ee648f926dc5237
Jean Osco <willjean29@gmail.com>
2024-03-21 11:30:05 -0500

    feat(ms-users): add register route and usecase

--- ms-users/src/infra/http/controllers/auth.controller.ts
+++ ms-users/src/infra/http/controllers/auth.controller.ts
@@ -3,14 +3,27 @@ import { TokenAdapter } from "@shared/adapters/token";
 import { configEnv } from "@shared/config";
 import { Request, Response, NextFunction } from "express";
 import { container } from "tsyringe";
+import { CreateUserUseCase } from "@app/create-user.usecase";
 
 export class AuthController {
+  private tokenAdapter = container.resolve(TokenAdapter);
   async login(req: Request, res: Response, next: NextFunction) {
     try {
       const createSession = container.resolve(CreateSessionUseCase);
       const user = await createSession.execute(req.body);
-      const tokenAdapter = container.resolve(TokenAdapter);
-      const token = tokenAdapter.sign({ id: user.id }, configEnv.TOKEN_SECRET_KEY, configEnv.EXPIRY_SECRET_KEY);
+      const token = this.tokenAdapter.sign({ id: user.id }, configEnv.TOKEN_SECRET_KEY, configEnv.EXPIRY_SECRET_KEY);
+      res.cookie("token", token, { httpOnly: true });
+      res.json(user);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async register(req: Request, res: Response, next: NextFunction) {
+    try {
+      const createUser = container.resolve(CreateUserUseCase);
+      const user = await createUser.execute(req.body);
+      const token = this.tokenAdapter.sign({ id: user.id }, configEnv.TOKEN_SECRET_KEY, configEnv.EXPIRY_SECRET_KEY);
       res.cookie("token", token, { httpOnly: true });
       res.json(user);
     } catch (error) {
--- ms-users/src/infra/http/routes/auth.routes.ts
+++ ms-users/src/infra/http/routes/auth.routes.ts
@@ -2,12 +2,14 @@ import { Router } from "express";
 import { validateDto } from "@infra/http/middlewares/validate-dto";
 import { AuthController } from "@infra/http/controllers/auth.controller";
 import { CreateSessionDto } from "@domain/entities/dtos/create-session.dto";
+import { CreateUserDto } from "@domain/entities/dtos/create-user.dto";
 
 const router = Router();
 export class AuthRoutes {
   static routes() {
     const authController = new AuthController();
     router.post("/login", validateDto(CreateSessionDto), authController.login);
+    router.post("/register", validateDto(CreateUserDto), authController.register);
     return router;
   }
 }
