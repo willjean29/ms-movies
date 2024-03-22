d5febb146da1cdf91706d4c422da9955b6084c6a
Jean Osco <willjean29@gmail.com>
2024-03-22 17:15:28 -0500

    fix(ms-users): fix de auth controller methods

--- ms-users/src/infra/http/controllers/auth.controller.ts
+++ ms-users/src/infra/http/controllers/auth.controller.ts
@@ -1,17 +1,17 @@
 import { CreateSessionUseCase } from "@app/create-session.usecase";
-import { TokenAdapter } from "@shared/adapters/token";
+import { ITokenAdapter, TokenAdapter } from "@shared/adapters/token";
 import { configEnv } from "@shared/config";
 import { Request, Response, NextFunction } from "express";
 import { container } from "tsyringe";
 import { CreateUserUseCase } from "@app/create-user.usecase";
 
 export class AuthController {
-  private tokenAdapter = container.resolve(TokenAdapter);
   async login(req: Request, res: Response, next: NextFunction) {
     try {
       const createSession = container.resolve(CreateSessionUseCase);
       const user = await createSession.execute(req.body);
-      const token = this.tokenAdapter.sign({ id: user.id }, configEnv.TOKEN_SECRET_KEY, configEnv.EXPIRY_SECRET_KEY);
+      const tokenAdapter = container.resolve(TokenAdapter);
+      const token = tokenAdapter.sign({ id: user.id }, configEnv.TOKEN_SECRET_KEY, configEnv.EXPIRY_SECRET_KEY);
       res.cookie("token", token, { httpOnly: true });
       res.json(user);
     } catch (error) {
@@ -23,7 +23,8 @@ export class AuthController {
     try {
       const createUser = container.resolve(CreateUserUseCase);
       const user = await createUser.execute(req.body);
-      const token = this.tokenAdapter.sign({ id: user.id }, configEnv.TOKEN_SECRET_KEY, configEnv.EXPIRY_SECRET_KEY);
+      const tokenAdapter = container.resolve(TokenAdapter);
+      const token = tokenAdapter.sign({ id: user.id }, configEnv.TOKEN_SECRET_KEY, configEnv.EXPIRY_SECRET_KEY);
       res.cookie("token", token, { httpOnly: true });
       res.json(user);
     } catch (error) {
