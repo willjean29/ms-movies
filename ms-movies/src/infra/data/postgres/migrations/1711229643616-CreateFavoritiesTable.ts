80f3fbbcf093edf1ed08fc0456f36a43ebdfcc17
Jean Osco <willjean29@gmail.com>
2024-03-23 16:40:35 -0500

    feat(ms-movies): add create favorites table migration

---
+++ ms-movies/src/infra/data/postgres/migrations/1711229643616-CreateFavoritiesTable.ts
@@ -0,0 +1,34 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateFavoritesTable1711229643616 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
+    await queryRunner.createTable(
+      new Table({
+        name: "favorites",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+            generationStrategy: "uuid",
+            default: "uuid_generate_v4()",
+          },
+          {
+            name: "user_id",
+            type: "int",
+          },
+          {
+            name: "is_active",
+            type: "boolean",
+            default: true,
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("favorites");
+  }
+}
